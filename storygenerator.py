4511988dbf9923703914e07b6fb4646845e95ad5
PascalHamar <s0573990@htw-berlin.de>
2023-12-28 19:38:05 +0100

    storygenerator geändert

--- storygenerator.py
+++ storygenerator.py
@@ -1,9 +1,8 @@
 # storygenerator.py
-import pip
-pip install openai
-from openai import OpenAI
+import openai
+import os
 
-client = OpenAI()
+openai.api_key = os.getenv['OPENAI_API_KEY']
 
 def generate_story_from_prompt(file_path):
     # Lesen des Prompts aus der Datei mit UTF-8-Kodierung
@@ -11,10 +10,10 @@ def generate_story_from_prompt(file_path):
         prompt = file.read()
 
     # Senden des Prompts an GPT-4 und Empfangen der Antwort
-    response = client.chat.completions.create(
+    response = openai.Completion.create(
         model="gpt-4",  # Sie können das gewünschte Modell hier spezifizieren
-        messages=[{"role": "system", "content": "Dies ist ein Story-Generator."},
-                  {"role": "user", "content": prompt}]
+        prompt=prompt,
+        max_tokens=150
     )
 
-    return response.choices[0].message.content
+    return response.choices[0].text.strip()
