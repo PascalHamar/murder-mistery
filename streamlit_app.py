ad70a4539123b09fdb5eb4bfe41cb943fa1af535
PascalHamar <s0573990@htw-berlin.de>
2023-12-28 10:59:42 +0100

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+
+def generate_story():
+    # Placeholder for story generation logic
+    return "Mystery Story", "Character List"
+
+def main():
+    st.title("Murder Mystery Game")
+
+    if st.button("Start Game"):
+        story, characters = generate_story()
+        st.write("Story: ", story)
+        st.write("Characters: ", characters)
+    else:
+        st.write("Click the button to start the game!")
+
+if __name__ == "__main__":
+    main()
 
