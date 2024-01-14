7924ec7ef7e0629416c99ff6213db50aeed0ff47
PascalHamar <s0573990@htw-berlin.de>
2024-01-14 22:41:51 +0100

    switch main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import gamecontroller as gc
 
-
+#'ONEPROMPT'
 def main():
     st.title("Murder Mystery Game Generator")
     # Initialisation
@@ -10,24 +10,18 @@ def main():
 
     # Start Game
     if st.session_state.game_status == 'init':
+        "Please enter your configurations:"
+        st.session_state.theme = st.text_input("Theme")
+        st.session_state.num_characters = st.number_input("Number of characters", min_value=3, max_value=10, step=1)
+        st.session_state.num_information = st.number_input("Information per character", min_value=1, max_value=10, step=1)
+        st.session_state.num_hints = st.number_input("Hints per character", min_value=1, max_value=10, step=1)
         "Click the button to generate the game!"
         if st.button("Generate Game"):
-            st.session_state.gamestory = gc.start_game()
-            st.session_state.game_status = 'story'
+            st.session_state.generated_content = gc.start_game_one_prompt()
+            st.session_state.game_status = 'content_generated'
+            st.session_state.generated_content
 
 
-    # Gameconfiguration
-    if st.session_state.game_status == 'story':
-        st.write(st.session_state.gamestory)
-        num_characters = st.number_input("Enter the number of players/characters:", min_value=3, max_value=10, step=1)
-
-        if st.button("Submit"):
-            st.session_state.characters = gc.create_characters(st.session_state.gamestory, num_characters)
-            st.session_state.game_status = 'characters'
-
-    # Display characters
-    if st.session_state.game_status == 'characters':
-        st.write(st.session_state.characters)
 
 
 if __name__ == "__main__":
